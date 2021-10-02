5a117b22cbd5402d5ecf3069b58413f41479a4c9
ltoler-lewis <78184776+ltoler-lewis@users.noreply.github.com>
2021-10-01 20:58:01 -0500

    Initial commit

---
+++ src/Character.java
@@ -0,0 +1,14 @@
+public abstract class Character{
+	int strength;
+	int intelligence;
+	int charisma;
+	int wisdom;
+	int dexterity;
+	int constitution;
+	
+	int level;
+	int xp;
+	
+	//Array of items since all characters can carry something
+	
+}
